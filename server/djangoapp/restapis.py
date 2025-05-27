f2c7b3280a54201328122ae12d1e5611350481f6
lowchiahui <138428818+lowchiahui@users.noreply.github.com>
2025-05-27 16:27:16 +0800

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -33,7 +33,7 @@ def get_request(endpoint, **kwargs):
 def analyze_review_sentiments(text):
     print("Loaded sentiment_analyzer_url:", sentiment_analyzer_url)
 
-    request_url = "https://sentianalyzer.1vx8o30kmvt9.us-south.codeengine.appdomain.cloud//analyze/" + text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url, verify=True)
